34648a1e048ff73c2b54b8be4e855b1be3cbdd6c
Muhammad Agung Rizkyana <muhammadagungrizkyana@Muhammads-Mac-mini.local>
2024-10-05 07:09:22 +0700

    feat: init materi

---
+++ src/models/category.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export interface Category {
+  _id: string;
+  name: string;
+  description: string;
+}
+
+const schema = new mongoose.Schema<Category>(
+  {
+    name: {
+      type: mongoose.Schema.Types.String,
+      required: true,
+    },
+    description: {
+      type: mongoose.Schema.Types.String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const CategoryModel = mongoose.model("category", schema);
+
+export default CategoryModel;
